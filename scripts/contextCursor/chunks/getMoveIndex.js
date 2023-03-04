6a4010447aa29fbabf6b27ac6e77ca0043986e1b
1izardo <daniel@dlazaro.ca>
2023-03-04 15:06:22 -0500

    Literally what am I doing?

--- scripts/contextCursor/chunks/getMoveIndex.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var getMoveIndex = function (mouseEventDirection, elPosition, elDimension, movementSpeed) {
-    var relativePos = mouseEventDirection - elPosition;
-    return (relativePos - elDimension / 2) / movementSpeed;
-};
-exports.default = getMoveIndex;
-//# sourceMappingURL=getMoveIndex.js.map
--- scripts/contextCursor/chunks/getStyleProp.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var getStyleProp = function (value) {
-    return getComputedStyle(document.documentElement).getPropertyValue(value);
-};
-exports.default = getStyleProp;
-//# sourceMappingURL=getStyleProp.js.map
